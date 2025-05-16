ea83615bdf7717840ddc3e312509253d8e4737b8
Nguyet Truong <nguyettruongthiminh@gmail.com>
2025-05-17 03:23:49 +0700

    change yml to run cicd

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: {
     baseURL: 'https://www.footballticketnet.com/champions-league/champions-league-final-2025',
-    headless: false,
+    headless: true,
     viewport: { width: 1280, height: 720 },
   },
   testDir: './tests',
