7dee3bf89667e77248f8d769913275f4433f6b0a
Nguyet Truong <nguyettruongthiminh@gmail.com>
2025-05-17 12:29:29 +0700

    run  Parallel cron job

---
+++ tests/newsletter-subscribe-negative.spec.ts
@@ -0,0 +1,38 @@
+// tests/newsletter.spec.ts
+import { test, expect } from '@playwright/test';
+import { NewsletterPage } from '../pages/newsletter.page';
+import { TestData } from '../utils/test-data';
+import { env } from '../utils/env'
+import { beforeEach, afterEach } from '../hooks/testHooks';
+
+test.describe('Newsletter Subscription Tests @smoke', () => {
+
+  let newsletterPage: NewsletterPage;
+
+  test.beforeEach(async ({ page }) => {
+    newsletterPage = new NewsletterPage(page);
+    await page.goto(env.baseURL);
+  });
+
+  test.beforeEach(async ({ page }, testInfo) => {
+    await beforeEach(page, testInfo);
+  });
+
+  test.afterEach(async ({ page }, testInfo) => {
+    await afterEach(page, testInfo);
+  });
+
+
+  test('Should show error with invalid email formats', async () => {
+    //subscribe with invalid mail
+    const invalidEmails = TestData.generateInvalidEmails();
+  
+    // Click subscribe button
+    await newsletterPage.subscribe(invalidEmails);
+    
+    // Assert the result
+    const failedMessage = await newsletterPage.getFailedMessage();
+    expect(failedMessage).toContain('Please Enter A Valid Email Address');
+
+  });
+});
--- tests/newsletter-subscribe.spec-positive.ts
+++ tests/newsletter-subscribe.spec-positive.ts
@@ -5,7 +5,7 @@ import { TestData } from '../utils/test-data';
 import { env } from '../utils/env'
 import { beforeEach, afterEach } from '../hooks/testHooks';
 
-test.describe('Newsletter Subscription Tests', () => {
+test.describe('Newsletter Subscription Tests @functional', () => {
 
   let newsletterPage: NewsletterPage;
 
@@ -22,6 +22,7 @@ test.describe('Newsletter Subscription Tests', () => {
     await afterEach(page, testInfo);
   });
 
+
   test('Successful subscription with valid name and email @positive', async () => {
     // Generate random email
     const testUser = TestData.generateUser();
@@ -34,16 +35,4 @@ test.describe('Newsletter Subscription Tests', () => {
     expect(successMessage).toContain('Message was sent succefully');
   });
 
-  test('Should show error with invalid email formats @negative', async () => {
-    //subscribe with invalid mail
-    const invalidEmails = TestData.generateInvalidEmails();
-  
-    // Click subscribe button
-    await newsletterPage.subscribe(invalidEmails);
-    
-    // Assert the result
-    const failedMessage = await newsletterPage.getFailedMessage();
-    expect(failedMessage).toContain('Please Enter A Valid Email Address');
-
-  });
 });
